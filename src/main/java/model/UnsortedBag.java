fed40138d3979cecc4e677d8520a27fe815d3d21
Justin Johnson <jjustin634@gmail.com>
2018-11-29 00:53:13 -0600

    adding UnsortedBag class to add items in the arrayList if user decides they want their list unordered

---
+++ src/main/java/model/UnsortedBag.java
@@ -0,0 +1,22 @@
+package model;
+
+import interfaces.IBag;
+import java.util.ArrayList;
+
+public class UnsortedBag implements IBag {
+
+    private ArrayList<Bag> data = new ArrayList<>();
+
+    // null values for now
+    public void add(E item) {
+    }
+    public E remove() {
+        return E;
+    }
+    public boolean contains(E item) {
+        return true;
+    }
+    public boolean empty() {
+        return true;
+    }
+}
