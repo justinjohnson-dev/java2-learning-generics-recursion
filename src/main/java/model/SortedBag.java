8478c0b476ede320be6a23eae83dde5811ef692e
Justin Johnson <jjustin634@gmail.com>
2018-11-29 00:53:29 -0600

    adding SortedBag class to add items in the arrayList if user decides they want their list ordered

---
+++ src/main/java/model/SortedBag.java
@@ -0,0 +1,23 @@
+package model;
+
+import interfaces.IBag;
+import java.util.ArrayList;
+
+public class SortedBag implements IBag {
+
+    private ArrayList<Bag> data = new ArrayList<>();
+
+    // null values for now
+    public void add(E item) {
+
+    }
+    public E remove() {
+        return E;
+    }
+    public boolean contains(E item) {
+        return true;
+    }
+    public boolean empty() {
+        return true;
+    }
+}
