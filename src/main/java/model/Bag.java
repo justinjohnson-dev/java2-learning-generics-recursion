2d7b0ece21573d038ef1e85964f796f096fb5cba
Justin Johnson <jjustin634@gmail.com>
2018-12-04 22:50:31 -0600

    adding all necessary loops to allow the user to add items to their bag, check if any item is in their bag, and finally to remove all elements in their bag.

--- src/main/java/model/Bag.java
+++ src/main/java/model/Bag.java
@@ -1,5 +1,6 @@
 package model;
 
+import interfaces.IBag;
 import java.util.Scanner;
 
 public class Bag {
@@ -7,18 +8,57 @@ public class Bag {
         run();
     }
 
-    public static void run() {
+    private static void run() {
         Scanner input = new Scanner(System.in);
         log("Which type of bag do you want? sorted/unsorted: ");
         String userChoice = input.next();
-        // decide whether to open SortedBag or UnsortedBag
 
+        IBag bag = createUserBag(userChoice);
+
+        log("");
         log("How many items to put in the bag? ");
-        int itemNumber = input.nextInt();
-        // will allow the user to add items into the bag using recursion on our add method
+        int numberOfItems = input.nextInt();
+        for (int i = 0; i <= numberOfItems -1; i++) {
+            log("Enter item " + (i+1) + ":");
+            String item = input.next();
+            bag.add(item);
+        }
+
+        log("");
+        log("You can check if something is in the bag.");
+        boolean isDone = true;
+        while(isDone) {
+            log("Check for (type done to stop): ");
+            String usersCheck = input.next();
+            if (usersCheck.equals("done")) {
+                isDone = false;
+            } else {
+                String msg = bag.contains(usersCheck)
+                        ? "Yes, it's in the bag."
+                        : "No, it's not in the bag.";
+                log("");
+                log(msg);
+            }
+        }
+
+        log("");
+        log("Let's remove stuff from the bag:");
+        while (!bag.empty()) {
+            if (!bag.empty()) {
+                log("Removing item: " + bag.remove());
+            }
+        }
     }
 
-    public static void log(String m) {
+    private static void log(String m) {
         System.out.println(m);
+    }
+
+    private static IBag createUserBag(String typeOfBag) {
+        if (typeOfBag.equals("sorted")) {
+            return new SortedBag<>();
+        } else {
+            return new UnsortedBag<>();
+        }
     }
 }
