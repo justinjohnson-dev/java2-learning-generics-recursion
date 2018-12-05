a9d20aa799380f89d8acb91d6eb8b2d83e613966
Justin Johnson <jjustin634@gmail.com>
2018-12-04 22:49:28 -0600

    adding the binary search needed to add user inputted elements into the bag alphabetically and a binary search for the contains method to check if a item is in the bag.

--- src/main/java/model/SortedBag.java
+++ src/main/java/model/SortedBag.java
@@ -2,22 +2,49 @@ package model;
 
 import interfaces.IBag;
 import java.util.ArrayList;
+import java.util.List;
 
-public class SortedBag implements IBag {
+public class SortedBag<E extends Comparable> implements IBag<E> {
+    private ArrayList<E> bag = new ArrayList<>();
 
-    private ArrayList<Bag> data = new ArrayList<>();
-
-    // null values for now
     public void add(E item) {
-
+        int position = recursiveBinarySearch(bag, item);
+        if (position >= 0) {
+            position += position;
+        } else {
+            position = (position * -1) -1;
+        }
+        bag.add(position, item);
     }
     public E remove() {
-        return E;
+        return bag.remove(0);
     }
     public boolean contains(E item) {
-        return true;
+        return recursiveBinarySearch(bag, item) >= 0;
     }
     public boolean empty() {
-        return true;
+        return bag.isEmpty();
+    }
+
+    private int recursiveBinarySearch(List<E> list, E key) {
+        int low = 0;
+        int high = list.size() - 1;
+
+        return recursiveBinarySearch(list, key, low, high);
+    }
+
+    private int recursiveBinarySearch(List<E> list, E key, int low, int high) {
+        if (low > high) {
+            return -low -1;
+        }
+
+        int mid = (low + high) / 2;
+        if (key.compareTo(list.get(mid)) < 0) {
+            return recursiveBinarySearch(list, key, low, mid - 1);
+        } else if (key.compareTo(list.get(mid)) == 0) {
+            return mid;
+        } else {
+           return recursiveBinarySearch(list, key, mid + 1, high);
+        }
     }
 }
